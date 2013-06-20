ce31ce5b31a203a4d7f9bb069fde7785478449cf
Robert Niederreiter <office@squarewave.at>
2013-06-20 14:40:03 +0200

    ticket occurrence basics

--- src/bda/plone/ticketshop/at/ticketoccurrence.py
+++ src/bda/plone/ticketshop/at/ticketoccurrence.py
@@ -1,7 +1,13 @@
-from AccessControl import ClassSecurityInfo
 from zope.interface import implementer
+from Acquisition import aq_parent
+from AccessControl import ClassSecurityInfo
+from DateTime import DateTime
 from Products.Archetypes import atapi
+from Products.CMFCore.permissions import ModifyPortalContent
 from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
+from plone.app.event.base import DT
+from plone.formwidget.datetime.at import DatetimeWidget
+from archetypes.referencebrowserwidget import ReferenceBrowserWidget
 from bda.plone.shop.interfaces import IBuyable
 from ..interfaces import ITicketOccurrence
 from ..config import PROJECTNAME
@@ -9,6 +15,44 @@ from .. import _
 
 
 schema = atapi.Schema((
+
+    atapi.ReferenceField('canonical_ticket',
+        widget=ReferenceBrowserWidget(
+            label=_(u'label_canonical_ticket', default=u'Canonical Ticket'),
+        ),
+        write_permission=ModifyPortalContent,
+        allowed_types=['Ticket'],
+        multiValued=0,
+        relationship='canonical_ticket',
+    ),
+
+    atapi.DateTimeField('startDate',
+        required=True,
+        searchable=False,
+        accessor='start',
+        write_permission=ModifyPortalContent,
+        languageIndependent=True,
+        widget=DatetimeWidget(
+            label=_(u'label_start', default=u'Start'),
+            description=_(u'help_start',
+                          default=u"Date and Time"),
+            with_time=1,
+            ),
+        ),
+
+    atapi.DateTimeField('endDate',
+        required=True,
+        searchable=False,
+        accessor='end',
+        write_permission=ModifyPortalContent,
+        languageIndependent=True,
+        widget=DatetimeWidget(
+            label=_(u'label_end', default=u'End'),
+            description=_(u'help_end',
+                          default=u"Date and Time"),
+            with_time=1,
+            ),
+        ),
 ))
 
 
@@ -23,5 +67,43 @@ class TicketOccurrence(atapi.BaseFolder, BrowserDefaultMixin):
     _at_rename_after_creation = True
     schema = TicketOccurrence_schema
     exclude_from_nav = True
+
+    def _dt_getter(self, field):
+        event = aq_parent(self)
+        timezone = event.getField('timezone').get(event)
+        dt = self.getField(field).get(self)
+        return dt.toZone(timezone)
+
+    def _dt_setter(self, fieldtoset, value, **kwargs):
+        if not isinstance(value, DateTime):
+            value = DT(value)
+        value = DateTime(
+            '%04d-%02d-%02dT%02d:%02d:%02d' % (
+                value.year(),
+                value.month(),
+                value.day(),
+                value.hour(),
+                value.minute(),
+                int(value.second())
+            )
+        )
+        self.getField(fieldtoset).set(self, value, **kwargs)
+
+    security.declareProtected('View', 'start')
+    def start(self):
+        return self._dt_getter('startDate')
+
+    security.declareProtected('View', 'end')
+    def end(self):
+        return self._dt_getter('endDate')
+
+    security.declareProtected(ModifyPortalContent, 'setStartDate')
+    def setStartDate(self, value, **kwargs):
+        self._dt_setter('startDate', value, **kwargs)
+
+    security.declareProtected(ModifyPortalContent, 'setEndDate')
+    def setEndDate(self, value, **kwargs):
+        self._dt_setter('endDate', value, **kwargs)
+
 
 atapi.registerType(TicketOccurrence, PROJECTNAME)
