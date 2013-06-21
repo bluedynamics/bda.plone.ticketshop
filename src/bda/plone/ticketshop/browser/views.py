6f5e57ee9dfa370b78086ba110b6c8522f72fd54
Robert Niederreiter <office@squarewave.at>
2013-06-21 12:54:27 +0200

    ticket listings as viewlets

--- src/bda/plone/ticketshop/browser/views.py
+++ src/bda/plone/ticketshop/browser/views.py
@@ -1,23 +1,33 @@
 from Acquisition import aq_parent
 from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.event.browser.event_view import EventView
 from bda.plone.shop.at import field_value
 from bda.plone.ticketshop.interfaces import ITicketOccurrenceData
 
 
-class BuyableEventView(EventView):
+class BuyableEventViewlet(ViewletBase):
+    index = ViewPageTemplateFile('tickets.pt')
 
     @property
     def tickets(self):
+        print 'BuyableEventViewlet'
         data = ITicketOccurrenceData(self.context)
+        print data.tickets
+        print '---'
         return data.tickets
 
 
-class BuyableEventOccurrenceView(EventView):
+class BuyableEventOccurrenceViewlet(ViewletBase):
+    index = ViewPageTemplateFile('tickets.pt')
 
     @property
     def tickets(self):
+        print 'BuyableEventOccurrenceViewlet'
         data = ITicketOccurrenceData(aq_parent(self.context))
+        print data.ticket_occurrences(self.context.id)
+        print '---'
         return data.ticket_occurrences(self.context.id)
 
 
