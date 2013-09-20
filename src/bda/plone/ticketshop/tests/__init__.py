4099326fed1ff78cefc8e68974bf9e0afc9ee152
Peter Holzer <hpeter@agitator.com>
2013-09-20 16:42:28 +0200

    added test setup

---
+++ src/bda/plone/ticketshop/tests/__init__.py
@@ -0,0 +1,37 @@
+from zope.interface import alsoProvides
+from plone.app.testing import (
+    IntegrationTesting,
+    PLONE_FIXTURE,
+    PloneSandboxLayer,
+)
+from bda.plone.ticketshop.interfaces import ITicketshopExtensionLayer
+
+
+def set_browserlayer(request):
+    """Set the BrowserLayer for the request.
+
+    We have to set the browserlayer manually, since importing the profile alone
+    doesn't do it in tests.
+    """
+    alsoProvides(request, ITicketshopExtensionLayer)
+
+
+class TicketshopLayer(PloneSandboxLayer):
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        import bda.plone.ticketshop
+        self.loadZCML(package=bda.plone.ticketshop,
+                      context=configurationContext)
+
+    def setUpPloneSite(self, portal):
+        self.applyProfile(portal, 'bda.plone.ticketshop:default')
+
+    def tearDownZope(self, app):
+        pass
+
+
+Ticketshop_FIXTURE = TicketshopLayer()
+Ticketshop_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(Ticketshop_FIXTURE,),
+    name="Ticketshop:Integration")
---
+++ src/bda/plone/ticketshop/tests/test_ticketshop.py
@@ -0,0 +1,17 @@
+import unittest2 as unittest
+from bda.plone.ticketshop.tests import (
+    Ticketshop_INTEGRATION_TESTING,
+    set_browserlayer,
+)
+
+
+class TestTicketshop(unittest.TestCase):
+    layer = Ticketshop_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        set_browserlayer(self.request)
+
+    def test_foo(self):
+        self.assertEquals(1, 1)
