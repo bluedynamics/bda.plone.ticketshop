31a9fb0a020ca7b5198b2df2ccb0d938a733462e
Jens W. Klein <jens@bluedynamics.com>
2014-01-22 15:18:39 +0100

    added test dependencies to setup

--- setup.py
+++ setup.py
@@ -12,42 +12,51 @@ longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 
 
-setup(name='bda.plone.ticketshop',
-      version=version,
-      description=shortdesc,
-      long_description=longdesc,
-      classifiers=[
-          'Environment :: Web Environment',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-      ],
-      author='BlueDynamics Alliance',
-      author_email='dev@bluedynamics.com',
-      license='GNU General Public Licence',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['bda', 'bda.plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Plone',
-          'plone.app.event[archetypes,ploneintegration]',
-          'collective.folderishtypes',
-          'bda.plone.shop',
-      ],
-      extras_require={
-          'test': [
-              'plone.app.testing',
-              'plone.app.contenttypes',
-              'Products.ATContentTypes',
-              'bda.plone.ticketshop',
+setup(
+    name='bda.plone.ticketshop',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    author='BlueDynamics Alliance',
+    author_email='dev@bluedynamics.com',
+    license='GNU General Public Licence',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['bda', 'bda.plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Plone',
+        'plone.app.event[archetypes,ploneintegration]',
+        'collective.folderishtypes',
+        'bda.plone.shop',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.contenttypes',
+            'Products.ATContentTypes',
+            'bda.plone.ticketshop',
+            'souper.plone[test]',
+            'bda.plone.cart[test]',
+            'bda.plone.checkout[test]',
+            'bda.plone.orders[test]',
+            'bda.plone.payment[test]',
+            'bda.plone.shipping[test]',
+            'bda.plone.shop[test]',
+            'bda.plone.ticketshop[test]',
           ]
-      },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
