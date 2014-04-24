3b6880c482e9fee0ff7322b84fb32f1e464714e7
Johannes Raggam <raggam-nl@adm.at>
2014-04-24 12:34:36 +0200

    add robot tests from bda.plone.shop

---
+++ src/bda/plone/ticketshop/tests/test_robot.py
@@ -0,0 +1,21 @@
+from bda.plone.ticketshop.tests import TicketshopAT_ROBOT_TESTING
+from plone.testing import layered
+
+import robotsuite
+import unittest
+
+try:
+    from plone.app.testing.interfaces import ROBOT_TEST_LEVEL
+except ImportError:
+    # don't fail if plone.app.testing < 4.2.4
+    ROBOT_TEST_LEVEL = 5
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.level = ROBOT_TEST_LEVEL
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite('robot'),
+                layer=TicketshopAT_ROBOT_TESTING),
+    ])
+    return suite
